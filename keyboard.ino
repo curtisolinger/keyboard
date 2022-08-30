0631a623edc3939792fa1fe57ff55b2ecbcea182
Curtis Olinger <curtisolinger@icloud.com>
2022-08-29 21:29:08 -0700

    Wrote the loop code and global declarations

--- keyboard.ino
+++ keyboard.ino
@@ -1,6 +1,14 @@
-void setup() {
-  // put your setup code here, to run once:
+// Declare an array of ints called buttons
+// int buttons[6];
 
+// Set the first element of the array to 2
+// int buttons[0] = 2;
+
+// Define an array of ints that represent the notes
+int notes[] = {262, 294, 330, 349};
+
+void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
