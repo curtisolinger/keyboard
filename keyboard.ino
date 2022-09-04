06e0bc33e539dfdb22704a5834645f8538fd2c6a
Curtis Olinger <curtisolinger@gmail.com>
2022-09-04 13:09:17 -0700

    Removed some old comments

--- keyboard.ino
+++ keyboard.ino
@@ -1,9 +1,3 @@
-// Declare an array of ints called buttons
-// int buttons[6];
-
-// Set the first element of the array to 2
-// int buttons[0] = 2;
-
 // Define an array of ints that represent the notes
 int notes[] = {262, 294, 330, 349};
 
