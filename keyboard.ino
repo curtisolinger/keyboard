589b8f4b576a78446e4caf5e1ca51e55d4f30dfc
Curtis Olinger <curtisolinger@gmail.com>
2022-09-04 12:36:06 -0700

    Completed code for project

--- keyboard.ino
+++ keyboard.ino
@@ -12,7 +12,30 @@ void setup() {
 }
 
 void loop() {
+  // Read the voltage value coming from the circut and store in keyVal
+  // the voltage will vary depending on what combination of resistors is activated
   int keyVal = analogRead(A0);
   Serial.println(keyVal);
 
+  // Determine the notes to play
+  if (keyVal == 1023) {
+    tone(8, notes[10]);
+  }
+
+  else if (keyVal >= 990 && keyVal <= 1010) {
+    tone(8, notes[1]);
+  }
+
+  else if (keyVal >= 505 && keyVal <= 515) {
+    tone(8, notes[2]);
+  }
+
+  else if (keyVal >= 5 && keyVal <= 10) {
+    tone(8, notes[3]);
+  }
+
+  else {
+    noTone(8);
+  }
+  
 }
